5649293f15c1ddffa62379907b322032508acf60
lucriver <lucrivDEV@gmail.com>
2022-01-01 22:23:18 -0600

    ...

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 //---
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 //--
 
 const uri = process.env.ATLAS_URI;
