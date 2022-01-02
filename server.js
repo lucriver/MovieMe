e7a90aef431673729ca66eb1133f6471637a5bac
lucriver <lucrivDEV@gmail.com>
2022-01-01 21:25:28 -0600

    Server.js corrections

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 //---
 app.use(cors());
@@ -32,6 +32,14 @@ app.use('/user', userRoutes);
 app.get((api_key_path), (req,res) => {
   res.send(api_key);
 })
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build/'));
+
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
