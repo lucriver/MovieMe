7995a439295147545969c3aa10183df2ff6570c6
lucriver <lucrivDEV@gmail.com>
2022-01-01 21:32:38 -0600

    debugging

--- client/src/components/SearchButton.js
+++ client/src/components/SearchButton.js
@@ -21,6 +21,8 @@ function SearchButton({ goSetTitle, goSetDate, goSetDescription, goSetCreator, g
   };
 
   function handleClick(){
+    console.log(tmdb_capacity);
+    console.log(key);
     goSetWatchList(false);
     switch(goFilter){
       case('Popular'):
